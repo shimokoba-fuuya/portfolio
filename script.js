397860ef4ba5a5f51060efb9c3ac8d9747deeba7
下木場風哉 <114757967+shimokoba-fuuya@users.noreply.github.com>
2022-10-02 17:51:32 +0900

    js修理
    slick修理しました

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+$('.slider').slick({
+    autoplay: true, //自動再生
+    autoplaySpeed: 2000, //自動再生のスピード
+    speed: 800, //スライドするスピード
+    dots: true, //スライドしたのドット
+    arrows: true, //左右の矢印
+    infinite: true, //スライドのループ
+    pauseOnHover: false, //ホバーしたときにスライドを一時停止しない　
+});
 jQuery(window).on('scroll', function() {
     if (100 < jQuery(this).scrollTop()) {
         jQuery('.menuback').addClass('.change-color');
@@ -26,16 +35,6 @@ $(function() {
     setTimeout(function() {
         $('.fadeout').fadeOut(1500);
     }, 2500);
-});
-
-$('.slider').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    speed: 800,
-    dots: true,
-    arrows: true,
-    infinite: true,
-    pauseOnHover: true,
 });
 
 $(function() {
